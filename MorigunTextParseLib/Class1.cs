e1cb407311dd6d1c461630e99026d1c7177abb47
Denis <gungrayv@gmail.com>
2018-11-28 15:10:58 +0300

    Добавьте файлы проекта.

---
+++ MorigunTextParseLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorigunTextParseLib
+{
+    public class Class1
+    {
+    }
+}
